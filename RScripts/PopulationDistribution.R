2ba7993f957c7b9ab7b108b1af37cff9cd3ef1eb
XanderHuggins <45410619+XanderHuggins@users.noreply.github.com>
2019-08-27 12:53:41 -0700

    Update PopulationDistribution.R

--- RScripts/PopulationDistribution.R
+++ RScripts/PopulationDistribution.R
@@ -9,43 +9,26 @@ library(reshape2)
 
 ## Import raw emerging water availability trends (from Rodell et al. 2018)
 # This requires the preprocessing of converting the provided .csv file to GeoTIFF
-GRACE <- raster("E:/! GIS_files/GRACE/Rodell_raw.tif")
+GRACE <- raster("Z:/2.active_projects/Xander/! GIS_files/GRACE/Rodell_raw.tif")
 
 # Resample GRACE (emerging trends) to 0.05d resolution
 s <- raster(GRACE) 
 res(s) <- 0.05
 EmergingTrend <- resample(GRACE, s, method='bilinear')
 # Write raster for future use w/o needing to resample
-writeRaster(EmergingTrend, "E:/! GIS_files/R_gis_exports/GRACE_0d05.tif", overwrite = TRUE)
+writeRaster(EmergingTrend, "Z:/2.active_projects/Xander/! GIS_files/GRACE/GRACE_coredata.tif", overwrite = TRUE)
 
 # Load raw population raster (GWPv4, 2015)
-Population_raw.2015 <- raster("E:/! GIS_files/PopulationRaster/2015/gpw-v4-population-count-rev11_2015_30_sec_tif/gpw_v4_population_count_rev11_2015_30_sec.tif")
+Population_raw.2015 <- raster("Z:/2.active_projects/Xander/! GIS_files/PopulationRaster/2015/gpw-v4-population-count-rev11_2015_30_sec_tif/gpw_v4_population_count_rev11_2015_30_sec.tif")
 # Resample population raster to 0.05d resolution
 Pop.2015 <- raster::aggregate(Population_raw.2015, fact = 6, fun = sum)
 # Write raster for future use w/o needing to resample
-writeRaster(Pop.2015, "E:/! GIS_files/R_gis_exports/POP_2015_0d05_V1.tif")
+writeRaster(Pop.2015, "Z:/2.active_projects/Xander/! GIS_files/R_gis_exports/POP_2015_0d05_V1.tif", overwrite = TRUE)
 
-# Load WRI Aqueduct raw data 
-Aqueduct <- readOGR(dsn = "E:/! GIS_files/Aqueduct",
-                    layer = "aqueduct_global_dl_20150409")
-# simplify attribute table to only shape and water stress attributes
-Aqueduct <- Aqueduct[,c(1:4,10:12)]
-
-# add unique integer ID for each BWS category
-Aqueduct$BWS_id <- ifelse(Aqueduct$BWS_cat == "1. Low (<10%)", 1,
-                          ifelse(Aqueduct$BWS_cat == "2. Low to medium (10-20%)", 2,
-                                 ifelse(Aqueduct$BWS_cat == "3. Medium to high (20-40%)", 3,
-                                        ifelse(Aqueduct$BWS_cat == "4. High (40-80%)", 4,
-                                               ifelse(Aqueduct$BWS_cat == "5. Extremely high (>80%)", 5,
-                                                      ifelse(Aqueduct$BWS_cat == "Arid & low water use", 0, 99))))))
-
-# write a new shapefile to use in converting vector file to raster at 0.05d resolution
-writeOGR(Aqueduct, dsn="E:/! GIS_files/Aqueduct/WaterStress_WRI.shp", 
-         layer = "WaterStress_WRI", driver="ESRI Shapefile", overwrite_layer=TRUE)
-# import converted water stress raster (GIS specific software perform vector rasterization much faster than in R)
-WaterStress <- raster("E:/! GIS_files/Aqueduct/WaterStress_WRI.tif")
+# Import converted Aqueduct (WRI, 2019) bws categorical raster (polygon file rasterized at 0.05d resolution in QGIS)
+WaterStress <- raster("Z:/2.active_projects/Xander/! GIS_files/QGIS_exports/Aqueduct2019_bwscat.tif")
 # set all cells containing NA to set ID for no data
-WaterStress[is.na(WaterStress[])] <- 99
+WaterStress[is.na(WaterStress[])] <- -99
 WaterStress %<>% as.factor()
 
 # split population raster by regions of water stress
@@ -68,13 +51,13 @@ Pop.Arid[]  <- 0
 Pop.NoDat[]  <- 0
 
 # populate population rasters per water stress class
-Pop.Low[WaterStress == 1]     <- Pop.2015[WaterStress == 1]
-Pop.LowMed[WaterStress == 2]  <- Pop.2015[WaterStress == 2]
-Pop.MedHigh[WaterStress == 3] <- Pop.2015[WaterStress == 3]
-Pop.High[WaterStress == 4]    <- Pop.2015[WaterStress == 4]
-Pop.ExHigh[WaterStress == 5]  <- Pop.2015[WaterStress == 5]
-Pop.Arid[WaterStress == 0]    <- Pop.2015[WaterStress == 0]
-Pop.NoDat[WaterStress == 99]  <- Pop.2015[WaterStress == 99]
+Pop.Low[WaterStress == 0]     <- Pop.2015[WaterStress == 0]
+Pop.LowMed[WaterStress == 1]  <- Pop.2015[WaterStress == 1]
+Pop.MedHigh[WaterStress == 2] <- Pop.2015[WaterStress == 2]
+Pop.High[WaterStress == 3]    <- Pop.2015[WaterStress == 3]
+Pop.ExHigh[WaterStress == 4]  <- Pop.2015[WaterStress == 4]
+Pop.Arid[WaterStress == -1]    <- Pop.2015[WaterStress == -1]
+Pop.NoDat[WaterStress == -99]  <- Pop.2015[WaterStress == -99]
 
 # Reclassify emerging trend into 0.1 cm/yr increment bins
 ReclassRanges <- data.frame(low = seq(-40, 6.0, 0.1), high = seq(-39.9, 6.1, 0.1), ReCLASS = seq(1:461))
@@ -100,23 +83,26 @@ Pop.distr.melted <- melt(Pop.distr, id.var = "Trend")
 ## plot results
 # For reference: bin ID vs emerging trend:
 # 390.5 = -1; 400.5 = 0; 410.5 = 1
-figure <- ggplot(Pop.distr.melted, aes(x = Trend, y = value, fill = variable)) +
+figure.A <- ggplot(Pop.distr.melted, aes(x = Trend, y = value, fill = variable)) +
   geom_bar(stat = "identity", position="stack", colour = "black") +
   scale_fill_manual(values = c("#ffffa3", "#ffe600", "#ff9a00", "#ff1900", "#bb0007", "#808080", "#252525")) + 
   scale_y_continuous(position = "right", breaks = seq(0, 500e6, by = 50e6), expand = c(0,0), 
                      sec.axis = dup_axis(name = element_blank())) +
   scale_x_continuous(limits = c(350.5, 430.5), breaks = seq(350.5, 430.5, by = 10), expand = c(0,0),  
                      labels = c("-5" ,"-4", "-3", "-2", "-1", "0", "1", "2", "3")) +
-  theme(panel.background = element_rect(fill = alpha("black", 0.03)),
+  theme(panel.background =  element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", colour = NA),
         panel.grid.minor.x = element_blank(), 
         panel.grid.minor.y = element_blank(),
-        panel.grid.major = element_line(alpha(colour = "black", 0.3), linetype = "dashed", size = 0.5),
+        panel.grid.major = element_line(alpha(colour = "white", 0.3), linetype = "dashed", size = 0.5),
         axis.line = element_line(colour = "black"),
         legend.position = "none") +
   geom_vline(xintercept = 400.5, size = 1.5, alpha = 0.8) +
   labs(x=expression(paste("Terrestrial Water Storage Rate of Change","  ","(cm", y^-1,")", sep="")),
        y = "Populaiton (in millions)") 
-figure
+figure.A
+
+ggsave("C:/Users/Tom/Desktop/PopulationDistr.png", figure.A, dpi = 500, width = 14, height = 10, bg = "transparent")
 
 ## Skewness calculation
 # create new dataframe 
@@ -127,3 +113,76 @@ SkewTable$Pop <- Pop.distr$Low + Pop.distr$LowMed + Pop.distr$MedHigh + Pop.dist
 SkewTable$PopSimp <- as.integer(round(SkewTable$Pop/100, 0))
 ExplodedSkewTable <- SkewTable[rep(rownames(SkewTable), SkewTable$PopSimp), ]
 skewness(ExplodedSkewTable$Trend)
+
+sum(SkewTable$Pop)
+Severe.Dry.pop <- SkewTable %>% filter(Trend < 380.5) %>% pull(Pop) %>% sum()
+Mod.Dry.pop    <- SkewTable %>% filter(Trend > 380.5 & Trend < 395.5) %>% pull(Pop) %>% sum()
+Neut.pop    <- SkewTable %>% filter(Trend > 395.5 & Trend < 405.5) %>% pull(Pop) %>% sum()
+Mod.Wet.pop    <- SkewTable %>% filter(Trend > 405.5 & Trend < 420.5) %>% pull(Pop) %>% sum()
+Sev.Wet.pop    <- SkewTable %>% filter(Trend > 420.5) %>% pull(Pop) %>% sum()
+
+
+# code to plot mean TWS trend per water stress class
+
+# Create raster of GRACE trend *times* population, for each water stress class
+GRACE.bwsLOW <- (Pop.Low * EmergingTrend) %>% as.data.frame()
+GRACE.bwsLOWMED <- Pop.LowMed * EmergingTrend %>% as.data.frame()
+GRACE.bwsMEDHIGH <- Pop.MedHigh * EmergingTrend %>% as.data.frame()
+GRACE.bwsHIGH <- Pop.High * EmergingTrend %>% as.data.frame()
+GRACE.bwsEXHIGH <- Pop.ExHigh * EmergingTrend %>% as.data.frame()
+GRACE.bwsARID <- Pop.Arid * EmergingTrend %>% as.data.frame()
+
+Pop.Low %<>% as.data.frame() 
+Pop.LowMed %<>% as.data.frame() 
+Pop.MedHigh %<>% as.data.frame()  
+Pop.High %<>% as.data.frame() 
+Pop.ExHigh %<>% as.data.frame() 
+Pop.Arid %<>% as.data.frame() 
+
+MeanTWS_low <- sum(GRACE.bwsLOW, na.rm = TRUE) / sum(Pop.Low, na.rm = TRUE)
+MeanTWS_lowmed <- sum(GRACE.bwsLOWMED, na.rm = TRUE) / sum(Pop.LowMed, na.rm = TRUE)
+MeanTWS_medhigh <- sum(GRACE.bwsMEDHIGH, na.rm = TRUE) / sum(Pop.MedHigh, na.rm = TRUE)
+MeanTWS_high <- sum(GRACE.bwsHIGH, na.rm = TRUE) / sum(Pop.High, na.rm = TRUE)
+MeanTWS_exhigh <- sum(GRACE.bwsEXHIGH, na.rm = TRUE) / sum(Pop.ExHigh, na.rm = TRUE)
+MeanTWS_arid <- sum(GRACE.bwsARID, na.rm = TRUE) / sum(Pop.Arid, na.rm = TRUE)
+
+a <- rbind(MeanTWS_low, MeanTWS_lowmed, MeanTWS_medhigh, MeanTWS_high, MeanTWS_exhigh)
+colnames(a) <- ("MeanTWS")
+a %<>% as.data.frame()
+a$Class <- seq(0, 4, by = 1)
+a$Pop <- c(sum(Pop.Low, na.rm = TRUE), sum(Pop.LowMed, na.rm = TRUE),
+           sum(Pop.MedHigh, na.rm = TRUE), sum(Pop.High, na.rm = TRUE),
+           sum(Pop.ExHigh, na.rm = TRUE))
+
+sum(a$Pop)
+
+progress <- data.frame(Class = c(1,2,3,4,5), Muman = c(0.154,0.001,-0.045,-0.090,-0.833), Pop = c(2438, 888, 922, 1108, 1711))
+progress
+
+Arid <- data.frame(Class = 6, Muman = -0.299, Pop = 79)
+
+figure.B <- ggplot() + 
+  theme(panel.background =  element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", colour = NA),
+        panel.grid.minor.x = element_blank(), 
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major = element_line(alpha(colour = "white", 0.3), linetype = "dashed", size = 0.5),
+        axis.line = element_line(colour = "black"),
+        axis.title = element_blank(),
+        legend.position = "none") +
+  geom_hline(yintercept = 0, size = 1.5) + 
+  stat_smooth(data = a, aes(x = Class, y = MeanTWS), lwd = 2, colour = "black", method = "lm", formula = y ~ poly(x,4), se = FALSE) +
+  # scale_colour_gradient2(low = "red 3", mid = "lemonchiffon2", high = "royalblue4",
+  #                        midpoint = -0.15, guide = FALSE)+
+  geom_point(data = a, aes(x = Class, y = MeanTWS, size = Pop, fill = ..y..), pch = 21, stroke = 1.5) + 
+  scale_fill_gradient2(low = "red 3", mid = "lemonchiffon2", high = "royalblue2",
+                       midpoint = -0.15, guide = FALSE)+ 
+  scale_radius("Population (millions)", range = c(6,15), trans = "sqrt") +  
+  scale_x_reverse(labels = c("0" = "Low", "1" = "Low to Med.", "2" = "Med. to High", "3" = "High", "4" = "Extremely High")) +
+  scale_y_continuous(limits = c(-1, 0.5), breaks = c(-1, -0.75, - 0.5, -0.25, 0, 0.25, 0.5), expand = c(0,0)) + 
+  # xlab("Baseline Water Stress Class") +
+  # ylab(expression(atop(bold('Population Weighted Mean Terrestrial'), paste(bold('Water Storage Rate of Change'~(cmy^-1)))))) + 
+  coord_flip()
+figure.B
+
+ggsave("C:/Users/Tom/Desktop/PopMeanTWS.png", figure.B, dpi = 500, width = 3.75, height = 12.2, bg = "transparent")
