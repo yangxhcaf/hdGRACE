55e69b15aa51013d4088d37ddf9d6fd673bdf234
XanderHuggins <45410619+XanderHuggins@users.noreply.github.com>
2019-09-10 14:18:38 -0700

    Update LandUse.R

--- RScripts/Figure 2/LandUse.R
+++ RScripts/Figure 2/LandUse.R
@@ -1,31 +1,23 @@
 library(raster)
 library(rgdal)
 library(dplyr)
-library(ggspatial)
 library(ggplot2)
-library(e1071)
+library(magrittr)
 library(scales)
 library(reshape2)
-library(naniar)
 library(RColorBrewer)
-library(Rmisc)
-library(forcats)
+library(Hmisc)
+library(quantreg)
+library(spatstat)
+library(reldist)
 
-### Load Data
+# Import Rodell et al. (2018) emerging freshwater availability trend as done in previous scripts
+EmergingTrend <- raster("Z:/2.active_projects/Xander/! GIS_files/R_gis_exports/GRACE_0d05.tif")
 
-# Load GRACE DATA (at 0.5 d resolution)
-GRACE <- raster("Z:/2.active_projects/Xander/! GIS_files/GRACE/Rodell_raw.tif")
-
-# Resample GRACE to 0.05d resolution
-s <- raster(nrow=3600, ncol=7200)
-GRACE_0d05res <- resample(GRACE, s, method='bilinear')
-GRACE_0d05 <- raster("Z:/2.active_projects/Xander/! GIS_files/GRACE/GRACE_coredata.tif")
-
-e <- extent(-180, 180, -90, 90)
-
-# Load Land Cover 2000 rater
+# Load land use raster
 LandUse <- raster("Z:/2.active_projects/Xander/! GIS_files/LandUse/GlobalLandUse_0d05.tif") 
 
+# create reclassification matrix to generalize land use to IPCC classes
 old <- c(10, 11, 12, 20, 30, 40, 
          50, 60, 61, 62, 70, 71, 72, 80, 81, 82, 90, 100, 160, 170, 
          110, 130,
@@ -34,7 +26,6 @@ old <- c(10, 11, 12, 20, 30, 40,
          120, 121, 122, 140, 150, 151, 152, 153,
          200, 201, 202,
          210)
-
 new <- c(1, 1, 1, 1, 1, 1, # agriculture
          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, # forest
          3, 3, # grassland
@@ -44,33 +35,39 @@ new <- c(1, 1, 1, 1, 1, 1, # agriculture
          7, 7, 7, # bare area
          8) # water
 Substitute_df <- data.frame(old, new)
-
 LandUse_CategoryBin <- subs(LandUse, Substitute_df, 1, 2, subsWithNA=TRUE)
 
-
+# load raster cell area in km2 at 0d05 resolution
+CellArea <- raster("Z:/2.active_projects/Xander/! GIS_files/R_gis_exports/WGS84_cellArea_0d05res.tif")
+CellArea_norm <- CellArea/max(CellArea[])
 
 # Merge GRACE data with binned species richness
 GRACE_df <- GRACE_0d05 %>% as.data.frame()
-GRACE_per_LandUseBin_df <- LandUse_CategoryBin %>% as.data.frame()
-GRACE_per_LandUseBin_df <- cbind(GRACE_df, GRACE_per_LandUseBin_df)
-colnames(GRACE_per_LandUseBin_df) <- c("GRACE", "LandUseCat")
-GRACE_per_LandUseBin_df <- GRACE_per_LandUseBin_df[complete.cases(GRACE_per_LandUseBin_df), ]
-GRACE_per_LandUseBin_df$LandUseCat <- as.factor(GRACE_per_LandUseBin_df$LandUseCat)
+LandUseBin_df <- LandUse_CategoryBin %>% as.data.frame()
+CellArea_norm_df <- CellArea_norm %>% as.data.frame()
+GRACE_LandUseBin_AreaWgt_df <- cbind(GRACE_df, LandUseBin_df, CellArea_norm_df)
+colnames(GRACE_LandUseBin_AreaWgt_df) <- c("GRACE", "LandUseCat", "AreaWgt")
+GRACE_LandUseBin_AreaWgt_df <- GRACE_LandUseBin_AreaWgt_df[complete.cases(GRACE_LandUseBin_AreaWgt_df), ]
+GRACE_LandUseBin_AreaWgt_df$LandUseCat <- as.factor(GRACE_LandUseBin_AreaWgt_df$LandUseCat)
 
-summary(GRACE_per_LandUseBin_df, 10)
-
-#colour brewer
-coul = brewer.pal(7, "Pastel1") 
-
-# coul = colorRampPalette(coul)(4)
-detach(package:Rmisc)
-detach(package:plyr)
-summ <- GRACE_per_LandUseBin_df %>% 
+# determine distribution statistics for the coupled raster grids
+summ <- GRACE_LandUseBin_AreaWgt_df %>% 
   group_by(LandUseCat) %>%
-  summarize(mean = mean(GRACE), median = median(GRACE), IQR = IQR(GRACE))
+  summarise(WeightedMean = weighted.mean(GRACE, AreaWgt),
+            WeightedMedian = weighted.median(GRACE, AreaWgt),
+            Weightedp25 = weighted.quantile(GRACE, AreaWgt, probs = 0.25),
+            Weightedp75 = weighted.quantile(GRACE, AreaWgt, probs = 0.75))
+summ$WeightedIQR <- summ$Weightedp75 - summ$Weightedp25
 
-### below is box plot
-LandUseBin_boxplot <- ggplot(GRACE_per_LandUseBin_df, aes(x=LandUseCat, y = GRACE)) +
+# format results for ggplot boxplot figure generation 
+summDF <- data.frame(x= summ$LandUseCat, min=summ$Weightedp25 - 1.5*summ$WeightedIQR, 
+                     low=summ$Weightedp25, wgt.mean = summ$WeightedMean, mid = summ$WeightedMedian, 
+                     top=summ$Weightedp75, max= summ$Weightedp75 + 1.5*summ$WeightedIQR)
+summDF$x %<>% as.factor()
+summDF <- summDF %>% filter(x != "8") # remove want land use from plot
+
+# plot figure
+Figure2d <- ggplot(summDF, aes(x = x, ymin = min, lower = low, middle = mid, upper = top, ymax = max, fill = x)) +
   geom_hline(yintercept = 0, size = 1.5) +
   geom_hline(yintercept = -3, size = 0.5, colour = "white", alpha = 0.5, linetype = "dashed") +
   geom_hline(yintercept = -2, size = 0.5, colour = "white", alpha = 0.5, linetype = "dashed") +
@@ -78,7 +75,7 @@ LandUseBin_boxplot <- ggplot(GRACE_per_LandUseBin_df, aes(x=LandUseCat, y = GRAC
   geom_hline(yintercept = 1, size = 0.5, colour = "white", alpha = 0.5, linetype = "dashed") +
   geom_hline(yintercept = 2, size = 0.5, colour = "white", alpha = 0.5, linetype = "dashed") +
   geom_hline(yintercept = 3, size = 0.5, colour = "white", alpha = 0.5, linetype = "dashed") +
-  geom_boxplot(outlier.alpha = 0, fill = "#A4A4A4") +
+  geom_boxplot(width = 0.75, lwd = 0.7, stat = "identity", outlier.alpha = 0, fill = "#A4A4A4") +
   scale_x_discrete(limits=c("7", "6", "3", "5", "1", "2", "4"),
                    labels = c("Bare Areas", "Shrubland/Sparse Vegetation", "Grassland", "Urban", 
                               "Agriculture", "Forest", "Wetland")) +
@@ -100,25 +97,9 @@ LandUseBin_boxplot <- ggplot(GRACE_per_LandUseBin_df, aes(x=LandUseCat, y = GRAC
         axis.text = element_text(face = "bold", size = 10, color = "black"),
         axis.title.y = element_text(color = "black")) +
   # scale_fill_manual(values = coul) +
-  coord_flip(ylim = c(-2, 2))
-LandUseBin_boxplot
+  coord_flip(ylim = c(-2, 2)) +
+  ylab("GRACE") + xlab("LandUseCat")
+Figure2d
 
-ggsave("C:/Users/Tom/Desktop/LandUseBin_boxplot.png",
-       LandUseBin_boxplot, width = 6, height = 7, dpi = 300, bg = "transparent")
-
-
-#
-# Rank1 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 1) %>% pull(GRACE) 
-# Rank2 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 2) %>% pull(GRACE) 
-# Rank3 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 3) %>% pull(GRACE) 
-# Rank4 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 4) %>% pull(GRACE) 
-# Rank5 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 5) %>% pull(GRACE)
-# Rank6 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 6) %>% pull(GRACE) 
-# Rank7 <- test1 %>% filter(Taxa == "Comb") %>% filter(Rank == 7) %>% pull(GRACE) 
-# 
-# t.test(Rank7, Rank6, "two.sided")
-# 
-# 
-# 
-# 
-# 
+ggsave("C:/Users/Tom/Desktop/CellArea_figures/LandUse_AreaWeighted.png", figure, 
+       dpi = 500, width = 6, height = 7, bg = "transparent")
